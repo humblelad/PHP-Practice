f7aa8c7fb54a3740bc0f45244854cb88d073e4ab
The Lost Jedi <32588696+robocyber@users.noreply.github.com>
2018-06-20 00:54:55 +0530

    learnt sting concat.

---
+++ second.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html>
+<head><title>This is my php website</title></head>
+<body>
+<h1>
+	<center><br>
+<?php
+//This is a comment :)
+echo "My first attempt with php";
+echo "My first attempt with php";
+
+$var1="Just a string";
+$var2=20;
+$var3=$var2; $var4=10;
+$var5=$var2+$var3+$var4;
+echo $var5;
+//string concat.
+$var6=" yeah!";
+$var5=$var1 .  $var6;
+echo $var5;
+
+?>
+</h1>
+</center>
+</body>
+</html>
