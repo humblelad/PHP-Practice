080185e89cf00bdcb54b2c0e33f72655e25d9523
The Lost Jedi <32588696+robocyber@users.noreply.github.com>
2018-06-19 22:58:58 +0530

    first simple hello world

---
+++ index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head><title>This is my php website</title></head>
+<body>
+<h1>
+	<center><br>
+<?php
+//This is a comment :)
+echo "My first attempt with php";
+?>
+</h1>
+</center>
+</body>
+</html>
